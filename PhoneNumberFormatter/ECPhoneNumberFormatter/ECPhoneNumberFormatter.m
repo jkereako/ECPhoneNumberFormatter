b01eb93a6e25f58d9efcad4b95472f8c62f88a4b
Jeff Kereakoglow <jeffrey.kereakoglow@noaa.gov>
2015-03-02 15:19:02 -0500

    Format: updated to follow NYT style guide
    - Used dot-syntax where ever possible
    - Placed brackets around all `if` statements
    - Placed `else` statements on new lines
    - Removed uneccessary white space

--- PhoneNumberFormatter/ECPhoneNumberFormatter/ECPhoneNumberFormatter.m
+++ PhoneNumberFormatter/ECPhoneNumberFormatter/ECPhoneNumberFormatter.m
@@ -24,42 +24,51 @@
 #import "ECPhoneNumberFormatter.h"
 
 @interface ECPhoneNumberFormatter()
+
 - (NSString *)parseString:(NSString *)input withFormat:(ECPhoneNumberFormat)phoneNumberFormat;
 - (NSString *)parseStringStartingWithOne:(NSString *)input withFormat:(ECPhoneNumberFormat)phoneNumberFormat;
 - (NSString *)parsePartialStringStartingWithOne:(NSString *)input withFormat:(ECPhoneNumberFormat)phoneNumberFormat;
 - (NSString *)parseLastSevenDigits:(NSString *)basicNumber;
-
 - (NSString *)stripNonDigits:(NSString *)input;
-- (NSUInteger)formattedNewLocationFromOldFormatted:(NSString *)formattedOld
-                                      formattedNew:(NSString *)formattedNew
-                              formattedOldLocation:(NSUInteger)formattedOldLocation
-                                       lengthAdded:(NSUInteger)lengthAdded;
+- (NSUInteger)formattedNewLocationFromOldFormatted:(NSString *)formattedOld formattedNew:(NSString *)formattedNew formattedOldLocation:(NSUInteger)formattedOldLocation lengthAdded:(NSUInteger)lengthAdded;
+
 @end
 
 @implementation ECPhoneNumberFormatter
 
 #pragma mark - NSFormatter
-
 - (NSString *)stringForObjectValue:(id)anObject {
     return [self stringForObjectValue:anObject withFormat:ECPhoneNumberFormatBrackets];
 }
 
 - (NSString *)stringForObjectValue:(id)anObject withFormat:(ECPhoneNumberFormat)phoneNumberFormat {
-    if (![anObject isKindOfClass:[NSString class]]) return nil;
-    if ([anObject length] < 1) return nil;
+    if (![anObject isKindOfClass:[NSString class]]) {
+        return nil;
+    }
 
-    NSCharacterSet *doNotWant = [[NSCharacterSet decimalDigitCharacterSet] invertedSet];
-    NSString *unformatted = [[(NSString *)anObject componentsSeparatedByCharactersInSet: doNotWant] componentsJoinedByString: @""];
-    if (unformatted.length == 0) return nil;
+    if (!((NSString *)anObject).length){
+        return nil;
+    }
+
+    NSCharacterSet *doNotWant = [NSCharacterSet decimalDigitCharacterSet].invertedSet;
+    NSString *unformatted = [[(NSString *)anObject componentsSeparatedByCharactersInSet:doNotWant] componentsJoinedByString: @""];
+
+    if (!unformatted.length) {
+        return nil;
+    }
 
     NSString *firstNumber = [unformatted substringToIndex:1],
     *output;
 
     if ([firstNumber isEqualToString:@"1"]) {
-        output = [self parseStringStartingWithOne:unformatted withFormat:phoneNumberFormat];
-    } else {
+        output = [self parseStringStartingWithOne:unformatted
+                                       withFormat:phoneNumberFormat];
+    }
+
+    else {
         output = [self parseString:unformatted withFormat:phoneNumberFormat];
     }
+
     return output;
 }
 
@@ -69,39 +78,43 @@
 }
 
 - (BOOL)isPartialStringValid:(NSString *__autoreleasing *)partialStringPtr proposedSelectedRange:(NSRangePointer)proposedSelRangePtr originalString:(NSString *)origString originalSelectedRange:(NSRange)origSelRange errorDescription:(NSString *__autoreleasing * __unused)error  {
-    NSString *formattedOld      = origString;
+
+    NSString *formattedOld = origString;
     NSString *proposedNewString = *partialStringPtr;
-    NSString *formattedNew      = [self stringForObjectValue:proposedNewString];
+    NSString *formattedNew = [self stringForObjectValue:proposedNewString];
     NSUInteger formattedLocationNew = 0;
-    NSUInteger lengthAdded          = 0;
+    NSUInteger lengthAdded = 0;
 
     if (formattedOld.length > proposedNewString.length) { // removing characters
         lengthAdded = -(origSelRange.location - (*proposedSelRangePtr).location);
-    } else if (formattedOld.length < proposedNewString.length) { // adding characters
+    }
+
+    else if (formattedOld.length < proposedNewString.length) { // adding characters
         lengthAdded = (*proposedSelRangePtr).location - origSelRange.location;
-    } else { // replace characters
+    }
+
+    else { // replace characters
         lengthAdded = origSelRange.length;
     }
 
-    formattedLocationNew = [self formattedNewLocationFromOldFormatted:formattedOld formattedNew:formattedNew formattedOldLocation:origSelRange.location lengthAdded:lengthAdded];
-
+    formattedLocationNew = [self formattedNewLocationFromOldFormatted:formattedOld
+                                                         formattedNew:formattedNew
+                                                 formattedOldLocation:origSelRange.location
+                                                          lengthAdded:lengthAdded];
     *partialStringPtr = formattedNew;
-    *proposedSelRangePtr = NSMakeRange(formattedLocationNew, (*proposedSelRangePtr).length);
+    *proposedSelRangePtr = NSMakeRange(formattedLocationNew,
+                                       (*proposedSelRangePtr).length);
 
     return NO;
 }
 
 #pragma mark - Private
-
 - (NSString *)stripNonDigits:(NSString *)input {
-    NSCharacterSet *doNotWant = [[NSCharacterSet decimalDigitCharacterSet] invertedSet];
-    return [[input componentsSeparatedByCharactersInSet: doNotWant] componentsJoinedByString: @""];
+    NSCharacterSet *doNotWant = [NSCharacterSet decimalDigitCharacterSet].invertedSet;
+    return [[input componentsSeparatedByCharactersInSet:doNotWant] componentsJoinedByString:@""];
 }
 
-- (NSUInteger)formattedNewLocationFromOldFormatted:(NSString *)formattedOld
-                                      formattedNew:(NSString *)formattedNew
-                              formattedOldLocation:(NSUInteger)formattedOldLocation
-                                       lengthAdded:(NSUInteger)lengthAdded {
+- (NSUInteger)formattedNewLocationFromOldFormatted:(NSString *)formattedOld formattedNew:(NSString *)formattedNew formattedOldLocation:(NSUInteger)formattedOldLocation lengthAdded:(NSUInteger)lengthAdded {
     NSUInteger unformattedLocationOld = [[self stripNonDigits:[formattedOld substringToIndex:formattedOldLocation]] length];
     NSUInteger unformattedLocationNew = unformattedLocationOld + lengthAdded;
     NSUInteger formattedLocationNew   = 0;
@@ -125,14 +138,19 @@
     if ([obj length] >= 4 && [obj length] <= 7) {
         [obj insertString:@"-" atIndex:3];
         output = obj;
-    } else if ([obj length] >= 8) { // We assume anything after the 7th digit is a phone number extension so add a " x" before it
+    }
+
+    else if ([obj length] >= 8) { // We assume anything after the 7th digit is a phone number extension so add a " x" before it
         [obj insertString:@"-" atIndex:3];
         [obj insertString:@" (x" atIndex:8];
         [obj insertString:@")" atIndex:[obj length]];
         output = obj;
-    } else {
+    }
+
+    else {
         output = obj;
     }
+
     return output;
 }
 
@@ -147,10 +165,14 @@
 
         if ( phoneNumberFormat == ECPhoneNumberFormatBrackets ) {
             output = [NSString stringWithFormat:@"(%@) %@", areaCode, lastSeven];
-        } else {
+        }
+
+        else {
             output = [NSString stringWithFormat:@"%@-%@", areaCode, lastSeven];
         }
-    } else if (len <= 10) {
+    }
+
+    else if (len <= 10) {
         output = [self parseLastSevenDigits:obj];
     }
 
@@ -168,9 +190,12 @@
 
     if ( phoneNumberFormat == ECPhoneNumberFormatBrackets ) {
         output = [NSString stringWithFormat:@"1 (%@)", partialAreaCode];
-    } else {
+    }
+
+    else {
         output = [NSString stringWithFormat:@"1-%@", partialAreaCode];
     }
+
     return output;
 }
 
@@ -180,12 +205,18 @@
 
     if (len == 1) {
         output = input;
-    } else if (len > 4) {
-        NSString *firstPart  = [self parsePartialStringStartingWithOne:[input substringToIndex:4] withFormat:phoneNumberFormat];
+    }
+
+    else if (len > 4) {
+        NSString *firstPart  = [self parsePartialStringStartingWithOne:[input substringToIndex:4]
+                                                            withFormat:phoneNumberFormat];
         NSString *secondPart = [self parseLastSevenDigits:[input substringFromIndex:4]];
         output = [NSString stringWithFormat:@"%@ %@", firstPart, secondPart];
-    } else {
-        output = [NSString stringWithFormat:@"%@", [self parsePartialStringStartingWithOne:input withFormat:phoneNumberFormat]];
+    }
+
+    else {
+        output = [NSString stringWithFormat:@"%@", [self parsePartialStringStartingWithOne:input
+                                                                                withFormat:phoneNumberFormat]];
     }
     
     return output;
